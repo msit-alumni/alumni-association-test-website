236ec12e20cd78d511e1b5d9b8eea830cdde182d
Utkarsh Ahuja <ahujautkarsh80@gmail.com>
2022-12-17 15:21:45 +0530

    react tailwind installed

---
+++ alumni-website/src/pages/AlumniProfiles/index.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import '../../../src/App.css'
+
+const ALumniProfiles = () => {
+  return (
+    <div>AlumniProfiles</div>
+  )
+}
+
+export default ALumniProfiles
---
+++ alumni-website/src/pages/Home/index.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import '../../../src/App.css'
+import Navbar from "../../components/common/Navbar"
+import Footer from "../../components/common/Footer"
+
+
+const Home = () => {
+  return (
+    <div>
+
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Footer */}
+      <Footer />
+
+    </div>
+  )
+}
+
+export default Home
