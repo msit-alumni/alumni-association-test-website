62ff1db5c2fb8743de3db7c6406c3fda53bf8fed
Utkarsh Ahuja <70762626+UtkarshAhuja2003@users.noreply.github.com>
2022-12-27 20:35:12 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "facebook":"#3B5998",
+        "google":"#666600",
+        'linkedin':"#0A66C2",
+         "theme":"#F5EBE0"
+      },
+      fontFamily:{
+        "Inter":"Inter"
+      },
+      screens: {
+  
+        'mmd': {'max': '767px'},
+        // => @media (max-width: 767px) { ... }
+      },
+      
+    },
   },
   plugins: [],
 }
