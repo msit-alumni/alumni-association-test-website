e3896fca5581c55eb9d4b5c2a0dc708480b11374
UdaySangwan30 <110191781+UdaySangwan30@users.noreply.github.com>
2023-03-24 19:53:51 +0530

    listview update

--- client/src/pages/Listview/index.jsx
+++ client/src/pages/Listview/index.jsx
@@ -65,10 +65,10 @@ export default function Index() {
                                 Listview.map((data) => (
 
 
-                                    <div className="w-full sm:w-1/2 md:w-1/2 xl:w-1/4 p-4">
+                                          <div className="w-full sm:w-1/2 md:w-1/2 xl:w-1/4 p-4">
                                         <a href="" className="c-card block bg-[#FFFFFF] shadow-md hover:shadow-xl  overflow-hidden">
                                             <div className="relative   pb-48 overflow-hidden">
-                                                <img className="absolute mx-auto inset-0 h-full w-full object-cover  "
+                                                <img className="absolute mx-auto object-cover w-40 h-40 mb-3 ml-12 mt-7 rounded-full shadow-lg just  "
                                                     src="https://images.hindustantimes.com/img/2022/12/10/550x309/FBL-WC-2022-MATCH57-NED-ARG-250_1670640281926_1670640281926_1670640316976_1670640316976.jpg"
                                                     alt="" />
                                             </div>
@@ -77,13 +77,11 @@ export default function Index() {
                                                 <h1 className="mt-2  text-xl  font-Merriweather font-bold">{data.name}</h1>
                                                 <h4 className="text-semibold text-lg font-Merriweather rounded-full text-[#3F72AF]">{data.course}</h4>
                                                 <div className="  items-center">
-                                                    <span className="text-md text-gray-700 font-semibold font-Merriweather ">
-                                                        {data.branch}</span>
+                                                    <span className="text-md text-gray-700 font-semibold font-Merriweather ">{data.branch}</span>
                                                     <p className=" text-gray-500 text-md font-Merriweather ">{data.city}</p>
 
-                                                    <div className="   mt-3 bg-[#FFFFFF]   ">
-                                                        <button
-                                                            className=" text-[#3F72AF]  text-xl  font-Merriweather py-3 px-3  font-Merriweather ">
+                                                    <div className="   mt-3 bg-[#FFFFFF]   hover:bg-[#3F72AF] text-[#3F72AF]  hover:text-white">
+                                                        <button className="  text-xl  font-Merriweather py-3 px-3  font-Merriweather ">
                                                             View Profile
                                                         </button>
                                                     </div>
