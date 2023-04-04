6f64d706d4ada0363f58cbd3a94583d84ec13c10
UtkarshAhuja2003 <ahujautkarsh80@gmail.com>
2023-04-04 13:37:45 +0530

    logo change

--- client/src/components/common/Navbar/index.jsx
+++ client/src/components/common/Navbar/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { FaBars, FaCaretDown } from "react-icons/fa";
-import Logo from "../../../assets/images/logo.png";
+import Logo from "../../../assets/images/logo.svg";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
