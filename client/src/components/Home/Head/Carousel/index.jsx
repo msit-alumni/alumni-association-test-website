c8b91f58d44dce5276add5626f551e689fb0fe94
UtkarshAhuja2003 <ahujautkarsh80@gmail.com>
2023-07-29 14:28:25 +0530

    home carousel

--- client/src/components/Home/Head/Carousel/index.jsx
+++ client/src/components/Home/Head/Carousel/index.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { BiCircle } from "react-icons/bi";
-
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
 
 function importAll(r) {
   let images = {};
@@ -12,51 +14,22 @@ function importAll(r) {
 const images=Object.values(data)
 
 const Events = () => {
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [intervalId, setIntervalId] = useState(null);
-    const [timeInterval, setTimeInterval] = useState(2000);
-  
-    const handleDotClick = (index) => {
-        clearInterval(intervalId);
-        setCurrentIndex(index);
-        setIntervalId(setInterval(() => {
-            setCurrentIndex((currentIndex + 1) % images.length);
-        }, timeInterval));
-    };
-    
-  
-    useEffect(() => {
-      const interval = setInterval(() => {
-        setCurrentIndex((prev) => (prev + 1) % images.length);
-      }, timeInterval);
-      setIntervalId(interval);
-      return () => clearInterval(interval);
-    }, [currentIndex, timeInterval]);
-
   return (
     <div>
       <div className="relative sm:w-[450px] md:w-[700px] lg:w-[750px] xl:w-[900px] lg:left-24">
-        <div className=" inset-0 flex items-center justify-center">
-          <img
-            className="object-cover object-center h-auto md:h-[540px] sm:w-[450px] md:w-[700px] lg:w-[750px] xl:w-[900px]"
-            src={images[currentIndex]}
-            alt=""
-          />
-        </div>
-        <div className="absolute bottom-0 left-0 right-0 flex justify-center">
-          {images.map((image, index) => (
-            <button
-              key={index}
-              className={`${
-                index === currentIndex ? "text-[#F9F7F7] bg-[#F9F7F7] "
-                : "text-[#F9F7F7] "
-              } m-2 md:text-[16px] text-[9px] rounded-full focus:outline-none focus:shadow-outline-black`}
-              onClick={() => handleDotClick(index)}
-            >
-              <BiCircle />
-            </button>
-          ))}
-        </div>
+      <OwlCarousel className='owl-theme' loop items={1} autoplay autoplayTimeout={2500} lazyLoad>
+        {
+          images.map((image, index) => (
+            <div key={index} className="w-full ease-in-out">
+              <img
+                className="w-full h-auto md:h-[540px] sm:w-[450px] md:w-[700px] lg:w-[750px] xl:w-[900px]"
+                src={image}
+                alt=""
+              />
+            </div>
+          ))
+        }
+      </OwlCarousel>
       </div>
     </div>
   );
