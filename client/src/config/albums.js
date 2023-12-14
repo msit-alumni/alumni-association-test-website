24871eae50c1d554d59985279ebd7e144259eecc
UtkarshAhuja2003 <ahujautkarsh80@gmail.com>
2023-12-14 20:10:27 +0530

    image fix

--- client/src/config/albums.js
+++ client/src/config/albums.js
@@ -2,39 +2,39 @@ import alum23 from "../assets/images/CarouselMain/img16.JPG.JPG"
 export const Albums = [
     {
         id: 1,
+        name: '',
+        title: 'Alumni Meet 2023',
+        desc: '',
+        image: alum23
+    },
+    {
+        id: 2,
         name: 'album2022',
         title: 'Alumni Meet 2022',
         desc: '',
         image: "https://drive.google.com/uc?export=view&pen?usp=forms_web&id=14PoP1OV0S_kYgVfcw0NzLR26FJx-Nm0q"
     },
     {
-        id: 2,
+        id: 3,
         name: 'alumni2012',
         title: 'Alumni Meet 2012',
         desc: '',
         image: "https://drive.google.com/uc?export=view&pen?usp=forms_web&id=1N_xLCWHnZGKUeYeNs8hqC-r8O92Y1UGM"
     },
     {
-        id: 3,
+        id: 4,
         name: 'alumni2016',
         title: 'Alumni Meet 2016',
         desc: '',
         image: "https://drive.google.com/uc?export=view&pen?usp=forms_web&id=160vZn1DgVlye_t1hE6QujGTj3dMr10Sb"
     },
     {
-        id: 4,
+        id: 5,
         name: 'alumni2021',
         title: 'Alumni Meet 2021',
         desc: '',
         image: "https://drive.google.com/uc?export=view&pen?usp=forms_web&id=1mimsuiDuF8bCwnV8gOreQtM9Ru_OE6GH"
-    },
-    {
-        id: 5,
-        name: 'alumni2023',
-        title: 'Alumni Meet 2023',
-        desc: '',
-        image: alum23
-    },
+    }
 
     
 ];
